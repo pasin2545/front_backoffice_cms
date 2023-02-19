e346ade28fab01954850c9530823c508155ca35a
pasin2545 <keawninpasin@gmail.com>
2023-02-19 14:52:15 +0700

    Co-authored-by: XZ0T1Cz- <NopasitJ@users.noreply.github.com> Co-authored-by: Pa Ri <PanchitaWallatri@users.noreply.github.com> Co-authored-by: Thanyalak A <Pthanzz@users.noreply.github.com> Co-authored-by: 0xPoom <underpoom@users.noreply.github.com> Co-authored-by: Tnp_Sr <Tnp-Sr@users.noreply.github.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { MuiTextField } from './components/MuiTextField';
 
 
 test('renders Create product', () => {
@@ -9,7 +10,7 @@ test('renders Create product', () => {
   expect(titleText).toBeInTheDocument();
 });
 
-test('should find all text fields in browser', () => {
+test('should have text in textfields', () => {
   render(<App />);
   //มี nameInput ?
   const nameInput = screen.getByLabelText('Name');
@@ -23,26 +24,77 @@ test('should find all text fields in browser', () => {
   //มี selectInput ?
   const selectInput = screen.getByLabelText('Select');
   expect(selectInput).toBeInTheDocument();
-
 });
 
+test('should have choose file button', () => {
+  render(<App />);
+  const ChooseFileButton = screen.getByLabelText('Choose File');
+  expect(ChooseFileButton).toBeInTheDocument();
+});
 
-test('should find value in text field when click button', () => {
-  const { getByText, getByLabelText } = render(<App />)
+describe('should find value in text field when click button', () => {
+  const { getByText, getByPlaceholderText } = render(<App />)
   const button = getByText('Submit')
-  const inputName = screen.getByLabelText('Name')
+  test('in NameInput', () => { 
+    const inputName = screen.getByText('Name')
+    fireEvent.click(button)
+    expect(inputName).toHaveValue()
+  })
+
+  test('in PriceInput', () => { 
+    const inputPrice = screen.getByText('Price')
+    fireEvent.click(button)
+    expect(inputPrice).toHaveValue()
+  })
+  
+  test('in PriceInput', () => { 
+    const inputPrice = screen.getByText('Price')
+    fireEvent.click(button)
+    expect(inputPrice).toHaveValue()
+  })
+
+  
+  
+  const inputAmount = screen.getByText('Amount')
+  const inputDescription = screen.getByText('Description')
+  
+  
+  expect(inputAmount).toHaveValue()
+  expect(inputDescription).toHaveValue()
+});
+
+test('should update catagory product on selecting a value from drop down', () => {
+  const { getAllByText } = render(<App />);
+  const Selectbar = getAllByText('Select')
+});
+
+test('should return bad respond when empty in text field', () => {
+  const { getByText, queryAllByText } = render(<App />);
+  const button = getByText('Submit')
   fireEvent.click(button)
-  expect(inputName).toBeChecked()
+  const errorN = screen.getByText(/Require/i);
+  expect(errorN).toBeInTheDocument();
+  
+});
+
+test('should return bad respond when number in NameInput', () => {
+  const { getByText, queryAllByText } = render(<App />);
+  const button = getByText('Submit')
+  fireEvent.click(button)
+  const errorN = screen.getByText(/Error/i);
+  expect(errorN).toBeInTheDocument();
+
 });
 
 
+// test('should update catagory product on selecting a value from drop down', () => {
+//   const { getAllByText } = render(<MuiTextField />);
+//   const Selectbar = getAllByText('Select')
+
+// });
 
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const titleText = screen.getByText(i);
 //   expect(titleText).toBeInTheDocument();
 // });
-
-test('dropdown',() =>{
-  
-})
