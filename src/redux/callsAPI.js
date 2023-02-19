7a1d9bacfbe433d6dd2168afe9bed20f39358d97
pasin2545 <keawninpasin@gmail.com>
2023-02-19 15:06:54 +0700

    add call api

--- src/redux/callsAPI.js
+++ src/redux/callsAPI.js
@@ -1,25 +1,27 @@
 import { publicRequest } from "./requestMethods";
 const api = publicRequest;
 export const Get_All_Product = async () => {
-
-    try{
-        
-        const res = await api.get('/showProducts').then(res=>console.log(res.data));
-           
+    try {
+        const res = await api.get('/showProducts').then(res => {
+            console.log(res);
+            console.log(res.data);
+        });
         return res
-    }catch (error){
+    } catch (error) {
         return 'failure'
     }
 }
 
 export const Add_product = async () => {
 
-    try{
-        
-        const res = await api.post('/createProduct');
-           
-        return res
-    }catch (error){
+    try {
+
+        const res = await api.post('/createProduct', {}).then(res => {
+            console.log(res);
+        });
+
+
+    } catch (error) {
         return 'failure'
     }
 }
